0a4368418c7ec813c8fa9f72d05441dc11773707
Toreakhmet <s.vasilovich@bk.ru>
2024-10-05 01:41:32 +0500

    add fix

--- sakoman/settings.py
+++ sakoman/settings.py
@@ -12,9 +12,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-o5^9scrjl$m)-0vt1d_7hkk0$=l^c90vx3ma@z&-8c!k+d7uzo'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["freelance.com.kz","www.freelance.com.kz"]
 
 
 # Application definition
